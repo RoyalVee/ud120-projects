db512841d52720c0c36d17da2a336fe397588982
Victor Oguche <victoroguche@gmail.com>
2020-06-19 10:01:16 +0100

    Update svm/svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -13,7 +13,9 @@
 import sys
 from time import time
 sys.path.append("../tools/")
-from email_preprocess import preprocess
+from tools.email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -26,7 +28,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf = SVC(kernel='linear', C= 10)
+clf.fit(features_train,labels_train)
 
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+
+print('Accuracy is {}'.format(acc))
 #########################################################
 
 
