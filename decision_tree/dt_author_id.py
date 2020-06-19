e3ae2021bf1a81e52bd64ff04b2aa16151256a44
Victor Oguche <victoroguche@gmail.com>
2020-06-19 10:04:21 +0100

    Update decision_tree/dt_author_id.py

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -13,7 +13,9 @@
 import sys
 from time import time
 sys.path.append("../tools/")
-from email_preprocess import preprocess
+from tools.email_preprocess import preprocess
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -26,7 +28,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf = tree.DecisionTreeClassifier(min_samples_split=50)
+clf.predict(features_train, labels_train)
 
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+
+print('Accuracy is {}'.format(acc))
 
 #########################################################
 
