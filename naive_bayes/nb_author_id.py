14509dcbf60ad6ceba31276149392ad16afbb9fd
Victor Oguche <victoroguche@gmail.com>
2020-06-19 09:53:30 +0100

    Update naive_bayes/nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,8 +14,9 @@
 import sys
 from time import time
 sys.path.append("../tools/")
-from email_preprocess import preprocess
-
+from tools.email_preprocess import preprocess
+from sklearn.naive_bayes import  GaussianNB
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -27,8 +28,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
 
+###predict using the model
+pred = clf.predict(features_test)
 
+##check accuracy
+acc = accuracy_score((pred, labels_test))
+print("Accuracy is {}".format(acc))
 #########################################################
 
 
