6d67d0b1792f1acdf945cfaa4b77877a0833dbf4
Victor Oguche <victoroguche@gmail.com>
2020-06-23 14:15:43 +0100

    update explore code base for Enron project

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -17,6 +17,61 @@
 
 import pickle
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+enron_data = pickle.load(open("../final_project/final_project_dataset_unix.pkl", "rb"))
+
+##number of data point in email + fin list
+print("The number of data point = {}".format(len(enron_data)))
+
+##number of features for each point
+num_feat = 0
+for keys in enron_data.keys():
+    num_feat = len(enron_data[keys])
+
+print("The number of features for each point = {}".format(num_feat))
+##number of POI == 1
+count = 0
+for keys in enron_data.keys():
+    if enron_data[keys]["poi"] == 1:
+        count += 1
+
+print("The number of POI = {}".format(count))
+
+##total value of stock for James Prentice
+print("total value of the stock belonging to James Prentice = {}".format(enron_data['PRENTICE JAMES']['total_stock_value']))
+
+##number of email messages we have from Wesley Colwell to persons of interest
+print("total number of emails from Wesley Colwell to persons of interest = {}".format(enron_data['COLWELL WESLEY']['from_this_person_to_poi']))
 
 
+##code base for searching enron data dictionary
+print(enron_data)
+names = list(enron_data.keys())
+for i in names:
+     if 'lay'.upper() in i:
+         print(i)
+         print(i,':', enron_data[i], '\n')
+
+##find POI with no salary record and POI with no email address
+no_salary = 0
+no_email = 0
+for keys in enron_data.keys():
+    if enron_data[keys]['salary'] == "NaN":
+        no_salary += 1
+    elif '.com' in enron_data[keys]['email_address']:
+        no_email += 1
+print(len(enron_data) - no_salary)
+print(no_email)
+
+##POI with no Total Payments
+num_no_total_pay = 0
+for keys in enron_data.keys():
+    if enron_data[keys]['total_payments'] == "NaN":
+        num_no_total_pay += 1
+print(num_no_total_pay)
+
+##number of POI == 1 and total payment = NaN
+count_POI_total_pay_NAN = 0
+for keys in enron_data.keys():
+    if enron_data[keys]["poi"] == 1 and enron_data[keys]["total_payments"] == "NaN":
+        count_POI_total_pay_NAN += 1
+print(count_POI_total_pay_NAN)
